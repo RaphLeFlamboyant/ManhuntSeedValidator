63f9ad16a434dd13bd5d95f47b2905c4ff646c5a
CANDELARESI Raphaël <32449035+RaphLeFlamboyant@users.noreply.github.com>
2023-09-25 22:46:20 +0200

    structure list

--- src/main/java/me/flamboyant/manhunt/seed_validator/Main.java
+++ src/main/java/me/flamboyant/manhunt/seed_validator/Main.java
@@ -4,12 +4,12 @@ import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public final class Main extends JavaPlugin implements CommandExecutor {
     private static List<Location> getCheckVillagePoints(World world) {
@@ -44,8 +44,8 @@ public final class Main extends JavaPlugin implements CommandExecutor {
 
     private void checkSeed(CommandSender sender) {
         World nether = getServer().getWorld("world_nether");
-        String message = "Fortress near 0 : ";
-        Location zero = new Location(nether, 0, 0, 0);
+        String message = "Fortress : ";
+        Location zero = new Location(nether, 0, 50, 0);
         Location fortressLocation = nether.locateNearestStructure(zero, StructureType.NETHER_FORTRESS, 22, false);
         if (zero.distance(fortressLocation) > 350)
             fortressLocation = null;
@@ -55,22 +55,29 @@ public final class Main extends JavaPlugin implements CommandExecutor {
         else
             message += "" + ChatColor.BLUE + ChatColor.BOLD + "OK : " + "\n";
 
-        message += ChatColor.RESET + "Village near 0 : ";
+        message += ChatColor.RESET + "--------\n";
+        message += ChatColor.RESET + "Check structures distances :\n";
 
-        HashSet<String> villageLocations = new HashSet<>();
-        int count = 0;
-        for (Location loc : getCheckVillagePoints(getServer().getWorld("world"))) {
-            Location villageLocation = nether.locateNearestStructure(loc, StructureType.VILLAGE, 10, false);
-            if (villageLocation != null && loc.distance(villageLocation) < 160 && villageLocations.add(villageLocation.toString()))
-                count++;
-        }
-
-        if (count > 0)
-            message += "" + ChatColor.BLUE + ChatColor.BOLD + count + "\n";
-        else
-            message += "" + ChatColor.RED + ChatColor.BOLD + "0" + "\n";
+        Map<String, StructureType> structureNameToStructureType = StructureType.getStructureTypes();
+        World world = getServer().getWorld("world");
+        Location worldZero = new Location(world, 0, 64, 0);
 
         if (sender != null) sender.sendMessage(message);
         else getServer().getConsoleSender().sendMessage(message);
+
+        int i = 1;
+        for (String structureName : structureNameToStructureType.keySet()) {
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                String msg2 = structureName + " : ";
+                Location structureLocation = world.locateNearestStructure(worldZero, structureNameToStructureType.get(structureName), 60, false);
+                if (structureLocation == null)
+                    msg2 += "" + ChatColor.RED + ChatColor.BOLD + "KO" + "\n";
+                else
+                    msg2 += "" + ChatColor.BLUE + ChatColor.BOLD + worldZero.distance(structureLocation) + " : " + "\n";
+
+                if (sender != null) sender.sendMessage(msg2);
+                else getServer().getConsoleSender().sendMessage(msg2);
+            }, 1 * i);
+        }
     }
 }
